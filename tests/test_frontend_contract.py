fb2798f2727953f1923ba461dd30ae98b54757dc
ymdog <ymkzdoginu11@gmail.com>
2026-09-19 00:49:50 +0900

    test: cover display wake on track change

--- tests/test_frontend_contract.py
+++ tests/test_frontend_contract.py
@@ -24,6 +24,14 @@ def test_now_playing_is_polled_only_by_app_module() -> None:
     assert "playpanel:now-playing" in progress_js
 
 
+def test_track_change_wakes_screen_and_resets_blank_timer() -> None:
+    app_js = read_static("static/js/app.js")
+    assert "wakeScreen();" in app_js
+    assert "if (trackChanged) {" in app_js
+    track_change_block = app_js.split("if (trackChanged) {", 1)[1].split("} else if", 1)[0]
+    assert track_change_block.index("wakeScreen();") < track_change_block.index("currentTrackKey = trackKey;")
+
+
 def test_lyrics_positive_offset_is_subtracted_from_audio_position() -> None:
     lyrics_js = read_static("static/js/lyrics.js")
     assert "getAudioPositionMs() - (syncOffsetSeconds * 1000)" in lyrics_js
